6b8ae6ae751697b7cde9f60373e8a4be63014e8d
KaioRodrigoDev <kaiorodrigocoder@gmail.com>
2022-07-03 04:26:55 -0300

    Criando componente Card

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, SafeAreaView } from 'react-native';
+import Card from './components/Card';
 
 export default function App() {
+
+  const cripto = [
+    {
+      name: 'Bitcoin',
+    },
+    {
+      name: 'Ethereum',
+    }
+
+  ]
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="#232630" barStyle="light-content" />
+      <TextInput style={styles.TextInput} />
+      {cripto.map((item, index) => {
+        return <Card key={index} content={item.name} />
+      }
+      )}
+    </SafeAreaView>
   );
 }
 
@@ -15,6 +31,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 30,
   },
+  TextInput: {
+    backgroundColor: '#121212',
+    width: '90%',
+    height: 50,
+    borderRadius: 25,
+    marginVertical: 10,
+    paddingHorizontal: 20,
+    color: '#fff',
+  }
 });
